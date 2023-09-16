490c3367f5adaf5c58257cb35def87e29e989d86
Aziza123 <azizaobryant@gmail.com>
2023-09-16 12:45:54 -0500

    style doc/ begin .js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log('pizza');
 
+const titleInput = document.getElementsByName("title")[0]; 
+const amountInput = document.getElementsByName("amount")[0]; 
+const dateInput = document.getElementsByName("date")[0];
+
+let addRow = document.querySelectorAll('tr')[1];
