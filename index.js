549f953291a7d19bfe8405a2468537fb7f5cd80b
Aziza123 <azizaobryant@gmail.com>
2023-09-16 23:27:46 -0500

    trial 1

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-console.log('pizza');
-
 const titleInput = document.getElementsByName("title")[0]; 
 const amountInput = document.getElementsByName("amount")[0]; 
 const dateInput = document.getElementsByName("date")[0];
+const submit = document.querySelectorAll('button')
 
+let dataInput = document.querySelectorAll('input');
 let addRow = document.querySelectorAll('tr')[1];
+  
+
+for (let i = 0; i < dataInput.length; ++i) {
+    
+    addRow[i].addEventListener('click', function(e) {
+      alert('youve sublitted')
+     
+    })
+
+
+}
+
+
