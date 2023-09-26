4f00a9fb559eee38bd083342972a2efb813b9ce4
Aziza123 <azizaobryant@gmail.com>
2023-09-25 22:35:13 -0500

    v2

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const submit = document.querySelector("#submit")
 
 let dataInput = document.querySelectorAll('input');
 let addRow = document.querySelectorAll('tr')[1];
+let table = document.querySelector("#mid-rowsss")
  
-function addRowsss() { 
+submit.addEventListener('click',function() {
     let titleInput = document.querySelectorAll("input")[0]
     let amountInput = document.querySelectorAll("input")[1]
-    let date =  document.querySelectorAll("input")[2];
+    let date =  document.querySelectorAll("input")[2]
     addRow.innerHTML = `
     <th>${titleInput.value}</th>
     <th>${date.value}</th>
     <th>${amountInput.value}</th>
     `
-}
-
-
-submit.addEventListener('click', addRowsss() )
+})
     
