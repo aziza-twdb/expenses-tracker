e3701d5243d1a329c89f27b2a65221d747478e24
Aziza123 <azizaobryant@gmail.com>
2023-10-11 23:09:43 -0500

    chast

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ var input = document.querySelectorAll('input');
 var addRow = document.querySelectorAll('tr');
 var table = document.getElementById("#datainject");
 
-submit.addEventListener('click',function() {
-    var midRow = document.createElement('tr')
-    midRow.classList.add('middle-rows')
+function injectNum() {
+    var midRow = document.createElement('tr');
+    midRow.classList.add('middle-rows');
     midRow.innerHTML = `
-    <td>${input[0].value}</td>
-    <td>${input[1].value}</td>
-    <td>${input[2].value}</td>
-    `
-    addRow[0].insertAdjacentElement("afterend", midRow)
-    input[0].value= ""
-    input[1].value= ""
-    input[2].value= ""
-
-    var calcRow = document.getElementsByClassName('middle-rows')[0].querySelector('td:nth-child(2)').textContent;
-    parseFloat(calcRow)
-    console.log()
-
-
-})
+      <td>${input[0].value}</td>
+      <td>${input[1].value}</td>
+      <td>${input[2].value}</td>
+    `;  
+ 
+    addRow[0].insertAdjacentElement("afterend", midRow);
+    input[0].value = "";
+    input[1].value = "";
+    input[2].value = "";
+}     
+ 
+  submit.addEventListener('click', injectNum);
+  
