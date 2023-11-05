7770e0c35ab9b589f400af152f3e1c1cdd626659
Aziza123 <azizaobryant@gmail.com>
2023-11-05 11:30:33 -0600

    part 2 trying to fix

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var input = document.querySelectorAll('input');
 var addRow = document.querySelectorAll('tr');
 var table = document.getElementById("#datainject");
 
+const Finalamount = document.querySelector("#datainject > tbody > tr.bottom-row > th:nth-child(2)");
+
 function injectNum() {
     var midRow = document.createElement('tr');
     midRow.classList.add('middle-rows');
@@ -13,10 +15,14 @@ function injectNum() {
     `;  
  
     addRow[0].insertAdjacentElement("afterend", midRow);
+
+
+    
     input[0].value = "";
     input[1].value = "";
     input[2].value = "";
 }     
  
   submit.addEventListener('click', injectNum);
+  
   
