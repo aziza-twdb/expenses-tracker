03ec99f624bda6c44b41398b42dbc1fd4a614874
Aziza123 <azizaobryant@gmail.com>
2024-02-17 11:52:08 -0600

    phase 2

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const submit = document.querySelector("#submit");
-var input = document.querySelectorAll('input');
-var addRow = document.querySelectorAll('tr');
-var table = document.getElementById("#datainject");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let input3 = document.getElementById("input3");
 
-const Finalamount = document.querySelector("#datainject > tbody > tr.bottom-row > th:nth-child(2)");
+function addExpense() {
+ alert('i clicked') 
+}
 
-function injectNum() {
-    var midRow = document.createElement('tr');
-    midRow.classList.add('middle-rows');
-    midRow.innerHTML = `
-      <td>${input[0].value}</td>
-      <td>${input[1].value}</td>
-      <td>${input[2].value}</td>
-    `;  
- 
-    addRow[0].insertAdjacentElement("afterend", midRow);
-
-
-    
-    input[0].value = "";
-    input[1].value = "";
-    input[2].value = "";
-}     
- 
-  submit.addEventListener('click', injectNum);
-  
+submit.addEventListener("onclick", addExpense())
   
