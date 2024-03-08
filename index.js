388afa7c7d50cb596e39db51be3e4a631752c107
Aziza O'Bryant <140858710+ahhzeeahh@users.noreply.github.com>
2024-03-08 15:40:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+
 const submit = document.querySelector("#submit");
-var input = document.querySelectorAll('input');
-var addRow = document.querySelectorAll('tr');
-var table = document.getElementById("#datainject");
-
-submit.addEventListener('click',function() {
-    var midRow = document.createElement('tr')
-    midRow.classList.add('middle-rows')
-    midRow.innerHTML = `
-    <td>${input[0].value}</td>
-    <td>${input[1].value}</td>
-    <td>${input[2].value}</td>
-    `
-    addRow[0].insertAdjacentElement("afterend", midRow)
-    input[0].value= ""
-    input[1].value= ""
-    input[2].value= ""
-
-    var calcRow = document.getElementsByClassName('middle-rows')[0].querySelector('td:nth-child(2)').textContent;
-    parseFloat(calcRow)
-    console.log()
+const final = document.querySelector("#final");
+var input = document.querySelectorAll('input')
+var amount = 0;
 
 
-})
+submit.addEventListener('click', function() { 
+
+
+    
+    var row = document.querySelectorAll('ul')
+
+        if (input[0].value == "" || input[1].value == "" ) {
+            alert("Please fill out required fields!")
+            input[1].classList.add("error")
+            input[0].classList.add("error")
+        } else {
+            console.warn("Submitted: " + input[0].value  + " for $" + input[1].value  + " on " + input[2].value  + " ")
+            input[1].classList.remove("error")
+            input[0].classList.remove("error")
+        }
+    
+  
+        for (var i = 0; i < input.length; i++) {
+
+            var list =  document.createElement('li');
+               list.textContent = input[i].value
+               row[i].appendChild(list);        
+        }
+
+        amount +=  parseFloat(input[1].value)
+        input[1].value = amount;
+        final.textContent = "$" + amount;
+
+        input[0].value = "";
+        input[1].value = "";
+        input[2].value = "";
+});
