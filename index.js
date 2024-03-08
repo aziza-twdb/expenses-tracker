d2caea1362724153403bd23037254ec6670f8316
azizaTWBD <aziza.obryant@twdb.texas.gov>
2024-03-08 09:30:52 -0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const submit = document.querySelector("#submit");
 
 
 submit.addEventListener('click', function() { 
-  
+
+    var list =  document.createElement('li');
     let input1 = document.getElementById("input1")
     let input2 = document.getElementById("input2")
     let input3 = document.getElementById("input3")
@@ -21,13 +22,13 @@ submit.addEventListener('click', function() {
     let title = document.getElementById("title")
     let num = document.getElementById("number")
     let dd  = document.getElementById("date")
-    var list =  document.createElement('li');
-
-    title.textContent = list(input1.value)
-    num.textContent = input2.value
-    dd.textContent = input3.value
+  
        
+        list.textContent = input1.value
+        title.appendChild(list)
 
+        num.textContent = input2.value
+        dd.textContent = input3.value
 
     
     input1.value = ""
