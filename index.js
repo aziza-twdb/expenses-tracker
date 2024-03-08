4d2416ad18f1b2a9070c966a70284efe03281715
azizaTWBD <aziza.obryant@twdb.texas.gov>
2024-03-08 15:20:42 -0600

    done

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 
 const submit = document.querySelector("#submit");
+const final = document.querySelector("#final");
+var input = document.querySelectorAll('input')
+var amount = 0;
 
 
 submit.addEventListener('click', function() { 
 
-    var list =  document.createElement('li');
-    let input1 = document.getElementById("input1")
-    let input2 = document.getElementById("input2")
-    let input3 = document.getElementById("input3")
 
-        if (input2.value == "" || input1.value == "" ) {
+    
+    var row = document.querySelectorAll('ul')
+
+        if (input[0].value == "" || input[1].value == "" ) {
             alert("Please fill out required fields!")
-            input1.classList.add("error")
-            input2.classList.add("error")
+            input[1].classList.add("error")
+            input[0].classList.add("error")
         } else {
-            console.warn("Submitted: " + input1.value  + " for $" + input2.value  + " on " + input3.value  + " ")
-            input1.classList.remove("error")
-            input2.classList.remove("error")
+            console.warn("Submitted: " + input[0].value  + " for $" + input[1].value  + " on " + input[2].value  + " ")
+            input[1].classList.remove("error")
+            input[0].classList.remove("error")
         }
     
-    let title = document.getElementById("title")
-    let num = document.getElementById("number")
-    let dd  = document.getElementById("date")
   
-       
-        list.textContent = input1.value
-        title.appendChild(list)
+        for (var i = 0; i < input.length; i++) {
 
-        num.textContent = input2.value
-        dd.textContent = input3.value
+            var list =  document.createElement('li');
+               list.textContent = input[i].value
+               row[i].appendChild(list);        
+        }
 
-    
-    input1.value = ""
-    input2.value = ""
-    input3.value = ""
+        amount +=  parseFloat(input[1].value)
+        input[1].value = amount;
+        final.textContent = amount;
 
+        input[0].value = "";
+        input[1].value = "";
+        input[2].value = "";
 });
-  
